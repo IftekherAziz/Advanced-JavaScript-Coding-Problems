14265de40d7706c0c381ed783d351fe54b4cce07
Iftekher Aziz <azizchy02@gmail.com>
2023-02-04 21:16:19 +0600

    update reverse.js

---
+++ discount.js
@@ -0,0 +1,10 @@
+/* 
+1. If ticket numbers is less than 100 , per ticket price is 100
+2. If ticket numbers is more  than 100 ,but less than 200 then first 100 ticket will be 100 tk per ticket
+and rest of the ticket will be 90 tk per ticket
+3. If you purchase more than 200 tickets 
+   first 100 ----> 100tk
+   1001-200  ----> 90tk
+   200+ ---------> 70tk
+*/
+
